4a0869804bd8bf3e314304bfc01975dc15526ce1
ne1sonngoo <nengo@ucsc.edu>
2025-01-15 10:33:02 +0900

    yes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -14,6 +14,10 @@ this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0
 this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0)
 // add rocket (p1)
 this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0)
+// add spaceships (x3)
+this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0)
+this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0)
+this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0)
   // define keys
 keyFIRE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)
 keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
@@ -23,5 +27,28 @@ keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
     update() {
         this.starfield.tilePositionX -= 4
         this.p1Rocket.update()
+        this.ship01.update()
+        this.ship02.update()
+        this.ship03.update()
+        if(this.checkCollision(this.p1Rocket, this.ship03)) {
+            console.log('kaboom ship 03')
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship02)) {
+            console.log('kaboom ship 02')
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship01)) {
+            console.log('kaboom ship 01')
+        }
     }
+    
+    checkCollision(rocket, ship) {
+        if (rocket.x < ship.x + ship.width && 
+        rocket.x + rocket.width > ship.x && 
+        rocket.y < ship.y + ship.height &&
+        rocket.height + rocket.y > ship. y) {
+        return true
+    } else {
+        return false
+    }
+}
 }
